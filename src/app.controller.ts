4bfd0f528d6cf0453dc434d5aed0e1e1c27367b0
vicenterg9 <vicenterg9@gmail.com>
2022-06-27 12:37:22 +0200

    feat: post

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from "@nestjs/common";
+import { Body, Controller, Get, HttpException, HttpStatus, Param, ParseIntPipe, Post, Query } from "@nestjs/common";
+import { get } from "http";
 import { AppService } from "./app.service";
 
 @Controller()
@@ -6,7 +7,99 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
+  public getHello(): string {
     return this.appService.getHello();
   }
+
+  @Post()
+  public postHello(@Body() name:any): string {
+    const nameString = JSON.stringify(name);
+    return nameString;
+  }
+
+  @Post("name")
+  public postHelloName(@Body() payload:{name:string}): string {
+
+    return `Hello ${payload.name}`;
+  }
+
+  @Get("/test")
+  public getTest() :string{
+    return "Hola Test";
+  }
+
+  @Get('/param/:id')
+  public getParam(@Param("id") id: string):string{
+    const type =  typeof id;
+    return `Param: ${id} of type ${type}` ;
+  }
+
+  @Get('/square/:someParam')
+  public getSquare(@Param("someParam")someParam: number) : string{
+    const type =  typeof someParam;
+    const square = someParam * someParam;
+    return `Square: ${someParam} of type ${type} is ${square}` ;
+  }
+
+  @Get('/square/pipe/:someParam')
+  public getSquarePipe(@Param("someParam", ParseIntPipe)someNumber: number) : string{
+    const type =  typeof someNumber;
+    const square = someNumber * someNumber;
+    return `Square: ${someNumber} of type ${type} is ${square}` ;
+  }
+
+  @Get('/square/Nan/:someParam')
+  public getSquareNan(@Param("someParam")someParam: number) : string{
+    const someNumber = parseInt(someParam.toString());
+    if (isNaN(someNumber)) throw new HttpException(`${someParam} is not a number`, HttpStatus.BAD_REQUEST);
+    const type =  typeof someNumber;
+    const square = someNumber * someNumber;
+    return `Square: ${someParam} of type ${type} is ${square}` ;
+  }
+
+
+  @Get('/multiply/:someParam/:otherNumber')
+  public getMultiply(
+    @Param("someParam", ParseIntPipe)someNumber: number,
+    @Param("otherNumber", ParseIntPipe)otherNumber: number,
+    ) : number{
+    const multiply = this.appService.multiply(someNumber, otherNumber);
+    return multiply ;
+  }
+
+  @Get('/multiply/query')
+  public getMultiplyQuery(
+    @Query("a", ParseIntPipe)someNumber: number,
+    @Query("b", ParseIntPipe)otherNumber: number,
+    ) : number{
+    return this.appService.multiply(someNumber, otherNumber) ;
+  }
+
+  @Get('/division/query')
+  public getDivisionQuery(
+    @Query("a", ParseIntPipe)someNumber: number,
+    @Query("b", ParseIntPipe)otherNumber: number,
+    ) : number{
+      try{
+        return this.appService.division(someNumber, otherNumber) ;
+
+      }catch(error){
+        throw new HttpException(error.message,HttpStatus.BAD_REQUEST);
+      }
+      
+  }
+
+  @Get('/raizCuadrada/query')
+  public getRaizCuadradaQuery(
+    @Query("a", ParseIntPipe)someNumber: number
+    ) : number{
+    try{
+      return this.appService.raizCuadrada(someNumber) ;
+
+    }catch(error){
+      throw new HttpException(error.message,HttpStatus.BAD_REQUEST);
+    }
+  }
+
+  
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,38 @@
-import { Injectable } from "@nestjs/common";
+import {   Injectable } from "@nestjs/common";
 
-@Injectable()
-export class AppService {
+
+export class AppServiceBase {
   getHello(): string {
     return "Hello World!";
   }
+
+  postHello(): string {
+    return "Hello World!";
+  }
+
+  public multiply(someNumber:number, otherNumber: number){
+    const multiply = someNumber * otherNumber;
+    return multiply ;
+  }
+  public division(dividendo:number, divisor: number){
+    
+    if(divisor!==0){
+      return  dividendo / divisor;
+    }else{
+       throw new Error(`${divisor} es cero `);
+    }
+  }
+
+  public raizCuadrada(someNumber:number){
+    if(someNumber >= 0){
+      return Math.sqrt(someNumber);
+    }else{
+      throw new Error(`${someNumber} es un numero negativo `);
+    }
+  }
+}
+
+@Injectable()
+export class AppService extends AppServiceBase {
+  
 }
